1efb6c5c06c3773819986fb5b35ac64e7d431d62
hadesyi <leesk55@gmail.com>
2018-12-12 02:04:36 +0900

    도전 03 두개의 이미지뷰에 이미지 번갈아 보여주기

--- app/src/main/java/com/tistory/hadeslee/dotimission01/MainActivity.java
+++ app/src/main/java/com/tistory/hadeslee/dotimission01/MainActivity.java
@@ -1,13 +1,42 @@
 package com.tistory.hadeslee.dotimission01;
 
+import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ScrollView;
 
 public class MainActivity extends AppCompatActivity {
+    ScrollView scrollView;
+    ImageView imageView;
+    ImageView imageView2;
+    BitmapDrawable bitmapDrawable;
+    int imageIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+        imageView2 = findViewById(R.id.imageView2);
+    }
+
+    public void onButton1Clicked(View v) {
+        changeImage();
+    }
+
+    private void changeImage() {
+        if (imageIndex == 0) {
+            imageView.setVisibility(View.VISIBLE);
+            imageView2.setVisibility(View.INVISIBLE);
+
+            imageIndex =1;
+        } else if (imageIndex == 1) {
+            imageView.setVisibility(View.INVISIBLE);
+            imageView2.setVisibility(View.VISIBLE);
+            imageIndex =0;
+        }
     }
 }
